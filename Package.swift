c536d79a89e70d0e49bc9123b4539566114160c3
vishal <vishal.p@questlabs.biz>
2025-01-25 15:37:09 +0530

    tag 1 vesrion

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "tag_testing_package",
+    platforms: [
+        .iOS(.v13) // Ensure compatibility with iOS 13 and up
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
---
+++ Sources/tag_testing_package/testing.swift
@@ -0,0 +1,22 @@
+//
+//  testing.swift
+//  tag_testing_package
+//
+//  Created by vishal on 25/01/25.
+//
+import SwiftUI
+
+struct tag1: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    tag1()
+}
