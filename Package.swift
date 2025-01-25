26271f6d81d48d5908303c9ad05f67b615777f98
vishal <vishal.p@questlabs.biz>
2025-01-25 15:34:08 +0530

    push the tag version 1

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "tag_testing_package",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "tag_testing_package",
+            targets: ["tag_testing_package"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "tag_testing_package"),
+
+    ]
+)
