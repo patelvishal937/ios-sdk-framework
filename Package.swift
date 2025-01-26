f3f98c46f00d9bf6ec623256f1c52974f0c5dd9d
vishal <vishal.p@questlabs.biz>
2025-01-26 22:28:55 +0530

    commit for the latest update

--- Package.swift
+++ Package.swift
@@ -4,24 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "tag_testing_package",
+    name: "quest_ios_sdk",
     platforms: [
         .iOS(.v13) // Ensure compatibility with iOS 13 and up
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "tag_testing_package",
-            targets: ["tag_testing_package"]),
+            name: "quest_ios_sdk",
+            targets: ["quest_ios_sdk"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "tag_testing_package"),
+    
         .binaryTarget(
             name: "quest_ios_sdk",
-            path: "./Sources/quest_ios_sdk")
+            path: "./Sources/quest_ios_sdk.xcframework")
 
     ]
 )
